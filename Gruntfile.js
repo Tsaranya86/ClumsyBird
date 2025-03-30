3b3ff002df2ed2bd808a374560d226bc0f686880
Tsaranya86 <saranya.t86@gmail.com>
2025-03-30 18:39:13 -0500

    dd

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+
 /*global module:false*/
 module.exports = function(grunt) {
     var sourceFiles = [
@@ -8,7 +9,6 @@ module.exports = function(grunt) {
         'js/screens/play.js',
         'js/screens/gameover.js',
     ];
-
     // Project configuration.
     grunt.initConfig({
         uglify: {
@@ -24,25 +24,21 @@ module.exports = function(grunt) {
                 }
             }
         },
-
         jshint: {
             options: {
                 jshintrc: ".jshintrc"
             },
-
             beforeConcat: {
                 files: {
                     src: sourceFiles
                 }
             },
-
             afterConcat: {
                 files: {
                     src: [ sourceFiles ]
                 }
             }
         },
-
         connect : {
             root : {
                 options : {
@@ -52,22 +48,51 @@ module.exports = function(grunt) {
                 }
             }
         },
-
         clean: {
             dist: [
-                'build/clumsy-min.js'
+                'build/clumsy-min.js',
+                'build/images'
             ],
         },
-
+        copy: {
+            images: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'images/',
+                        src: ['**/*.png'],
+                        dest: 'build/images/'
+                    }
+                ]
+            }
+        },
+        
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.png'],
+                    dest: 'build/images/'
+                }]
+            }
+        },
     });
-
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks("grunt-contrib-connect");
-
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
     // Default task.
     grunt.registerTask('default', ['uglify']);
     grunt.registerTask('lint', ['jshint:beforeConcat', 'concat', 'jshint:afterConcat']);
+    // Clean build directory.
+    grunt.registerTask('clean-build', ['clean:dist']);
+    // Copy PNG files to build directory.
+    grunt.registerTask('copy-images', ['copy:images']);
+    // Optimize PNG files and copy to build directory.
+    grunt.registerTask('optimize-images', ['imagemin']);
+    
 };
